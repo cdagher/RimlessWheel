40bb867cb85292e564e6f8ed1dd1bc7d8b684aca
Chris Dagher <chrisdagher642@gmail.com>
2022-09-12 13:15:46 -0600

    Intrgrated some ROS stuff, need to handle the subscriber method still.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,35 @@
 #include <Arduino.h>
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/JointState.h>
 #include <Wire.h>
 #include <HardwareSerial.h>
 #include <ODriveArduino.h>
+#include <Adafruit_LSM6DSOX.h>
+
 
 // print stream operator helper functions
 template<class T> inline Print& operator <<(Print &obj,     T arg) { obj.print(arg);    return obj; }
 template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(arg, 4); return obj; }
 
 
-#define MOTOR_VELOCITY_LIMIT 10.0 // radians per second?
+#define MOTOR_SUBSCRIBER_NAME "/motors"
+#define ENCODER_PUBLISHER_NAME "/sensors"
+
+#define MOTOR_VELOCITY_LIMIT 10.0 // radians per second? Maybe rotations per second?
 #define MOTOR_CURRENT_LIMIT  20.0 // amps
+
+
+ros::NodeHandle nh;
+
+void receiveJointState(const sensor_msgs::JointState &msg);
+sensor_msgs::JointState motorStates; // comands for the motors
+ros::Subscriber<sensor_msgs::JointState> motors(MOTOR_SUBSCRIBER_NAME, &receiveJointState);
+
+void populateSensorStates();
+sensor_msgs::JointState sensorStates; // feedback of the encoder positions
+ros::Publisher sensors(ENCODER_PUBLISHER_NAME, &sensorStates);
+
 
 // Teensy 3 and 4 (all versions) - Serial1
 // pin 0: RX - connect to ODrive TX (GPIO1)
@@ -19,12 +39,29 @@ HardwareSerial &odriveSerial = Serial1;
 
 // ODrive object
 ODriveArduino ODrive(odriveSerial);
-
-
 void calibrateMotor(bool motor);
 
+Adafruit_LSM6DSOX imu;
+data_rate imuDataRate = LSM6DS_RATE_1_66K_HZ;
+accel_range imuAccelRange = LSM6DS_ACCEL_RANGE_4_G;
+gyro_range imuGyroRange = LSM6DS_GYRO_RANGE_500_DPS;
+
 void setup() {
-    // ODrive uses 115200 as the baudrate
+
+  nh.initNode();
+  nh.subscribe(motors);
+  nh.advertise(sensors);
+
+  if (!imu.begin_I2C()) {
+    while (1) ; // hang b/c we need the IMU
+  }
+  imu.setAccelDataRate(imuDataRate);
+  imu.setGyroDataRate(imuDataRate);
+  imu.setAccelRange(imuAccelRange);
+  imu.setGyroRange(imuGyroRange);
+
+
+  // ODrive uses 115200 as the baudrate
   odriveSerial.begin(115200);
 
   // Serial output over USB
@@ -50,8 +87,37 @@ void setup() {
   Serial.println("Ready!");
 }
 
-void loop() {
+void loop() {    
+    sensors.publish(&sensorStates);
+    nh.spinOnce();
+}
 
+void receiveJointState(const sensor_msgs::JointState &msg) {
+    // TODO: actually write this method
+    // received joint state should be something like:
+    // { torque0, velocity0, torque1, velocity1 }
+    // where the 1st two values are for motor 0 and the 2nd two are for motor 1.
+    // One of these values per motor should be zero, and will be ignored
+    return;
+}
+
+void populateSensorStates() {
+    float encPos0 = ODrive.GetPosition(0);
+    float encPos1 = ODrive.GetPosition(1);
+
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t temp;
+    imu.getEvent(&accel, &gyro, &temp);
+
+    sensorStates.position_length = 8;
+
+    float sensorStateVals[8] = 
+        {
+        encPos0, encPos1, // encoder values
+        accel.acceleration.x, accel.acceleration.y, accel.acceleration.z, // accelerometer values
+        gyro.gyro.x, gyro.gyro.y, gyro.gyro.z // gyroscope values
+        };
 }
 
 void calibrateMotor(bool motornum) {
