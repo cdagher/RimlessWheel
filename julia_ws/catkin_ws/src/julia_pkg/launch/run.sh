adb817cbcc389d5286862b99b46e42d5e0e814ca
nardosashenafi <nardosashenafi@u.boisestate.edu>
2022-12-05 14:47:35 -0700

    julia fixes

--- julia_ws/catkin_ws/src/julia_pkg/launch/run.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-
-julia_script="$JULIA_PROJECT/evaluateController.jl"
-julia $julia_script $SAVED_WEIGHT_DIR
-
--- julia_ws/catkin_ws/src/julia_pkg/src/evaluateController.jl
+++ julia_ws/catkin_ws/src/julia_pkg/src/evaluateController.jl
@@ -1,3 +1,7 @@
+#!/usr/bin/env julia
+using Pkg
+Pkg.activate(".")
+
 using DiffEqFlux
 using LinearAlgebra, BSON
 using RobotOS
@@ -11,14 +15,15 @@ const l1  = 0.26f0                  #wheel
 const l2  = 0.05f0                  #torso
 const k   = 10
 const α   = Float32(360.0/k/2.0 * pi/180.0)
+const DEG_TO_RAD = pi/180.0
 
 unn  = FastChain(FastDense(6, 8, elu), 
                 FastDense(8, 5, elu),
                 FastDense(5, 1))
 
-inputLayer(x) = [cos(x[3]), sin(x[3]), cos(x[4]), sin(x[4]), x[7], x[8]]
+inputLayer(x) = [cos(x[1]), sin(x[1]), cos(x[2]), sin(x[2]), x[3], x[4]]
 
-ps = BSON.load("nnController_viaLCP_6-8-8-5-5-1elu.bson")[:param]
+ps = BSON.load("saved_weights/nnController_viaLCP_6-8-8-5-5-1elu.bson")[:param]
 
 function initialState(θ0, θ0dot, ϕ0, ϕ0dot)
     @assert pi-α <= θ0 <= pi+α "Give an initial spoke angle for the spoke in contact. This will help set the rimless wheel in contact with the surface"
@@ -35,25 +40,33 @@ function initialState(θ0, θ0dot, ϕ0, ϕ0dot)
     return [x, y, ϕ, θ, xdot, ydot, ϕdot, θdot]
 end
 
+function update_state!(msg::JointState, state::Vector)
+    state[1] = msg.position[1]*DEG_TO_RAD
+    state[2] = msg.position[2]*DEG_TO_RAD
+    state[3] = msg.velocity[1]*DEG_TO_RAD
+    state[4] = msg.velocity[2]*DEG_TO_RAD
+end
+
 function main()
-    @info "Julia started with weights located at $(weightdir)."
     init_node("nn_controller")
     state = zeros(Float64,4)
-    pub = Publisher{Float64Msg}("torso/command", queue_size=1)
-    sub = Subscriber{JointState}("/joint_states", update_state!, (state,), queue_size=1)
+    pub = Publisher{JointState}("/torso_command", queue_size=1)
+    sub = Subscriber{JointState}("/sensors", update_state!, (state,), queue_size=1)
     @info "ROS node initialized. Loading models..."
 
     x0 = initialState(pi, -1.0f0, 0.0f0, 0.0f0)
     torque = unn(inputLayer(x0), ps)[1]
 
     @info "Model loaded. Spinning ROS..."
-    loop_rate = Rate(800.0)
+    loop_rate = Rate(1000.0)
+    torque_msg = JointState();
     while !is_shutdown()
-        header = std_msgs.msg.Header()
-        header.stamp = RobotOS.now()
-        torque = compute_control(state, policy)
-        cmd = Float64Msg(torque)
-        publish(pub, cmd)
+        torque_msg.header = std_msgs.msg.Header()
+        torque_msg.header.stamp = RobotOS.now()
+        torque = unn(inputLayer(state), ps)[1]
+        torque_msg.effort = zeros(1)
+        torque_msg.effort[1] = torque
+        publish(pub, torque_msg)
         rossleep(loop_rate)
     end
     safe_shutdown_hack()
@@ -61,12 +74,12 @@ end
 
 function safe_shutdown_hack()
     @info "Publishing zero torque."
-    run(`rostopic pub -1 /torso/command std_msgs/Float64 "data: 0. "`, wait=false);
+    run(`rostopic pub -1 /torso_command sensor_msgs/JointState "effort: [0.] "`, wait=false);
 end
 
 Base.atexit(safe_shutdown_hack)
 
-# if !isinteractive()
-#     @info "Julia packages loaded. Starting main()"
-#     main()
-# end
+if !isinteractive()
+    @info "Julia packages loaded. Starting main()"
+    main()
+end
