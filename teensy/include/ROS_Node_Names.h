0e66dc193524e4ffec62b83fc7e91cb8d4759625
Chris Dagher <chrisdagher642@gmail.com>
2023-02-04 20:27:15 -0700

    Separated node names into a header

---
+++ teensy/include/ROS_Node_Names.h
@@ -0,0 +1,4 @@
+#define MOTOR_SUBSCRIBER_NAME "/torso_command"
+#define ODRIVE_SUBSCRIBER_NAME "/odrive_command"
+#define ENCODER_PUBLISHER_NAME "/sensors"
+#define ODRIVE_ERROR_PUBLISHER_NAME "/odrive_errors"
